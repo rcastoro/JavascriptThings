9bc676a38a273baf29fa40b397ac33bb3e7b10be
RCAST <rcastoro@email.itt-tech.edu>
2018-05-20 14:46:32 -0400

    Updates to server app js to pass around objects and store puzzle pieces on server to allow new clients connecting to view all previous puzzle moves

--- SocketNode_PuzzleApp/index.js
+++ SocketNode_PuzzleApp/index.js
@@ -6,6 +6,11 @@ var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//Properties
+var usersOnline = 0;
+var puzzlePieces = [];
+var puzzlePiece;
+
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res)
@@ -18,31 +23,58 @@ imgs = imageLoader.GetImages();
 // var isLoaded = 0;
 
 io.on('connection', function(socket) {
-
-	// if(isLoaded == 0)
-	// {
-		
-		console.log("img length is 0");
-		console.log(imgs.length);
-		for(var x=0;x<imgs.length;x++)
+	
+	for(var x=0;x<imgs.length;x++)
+	{
+		puzzlePiece = new Object();
+		//Save initial server version of puzzle pieces into array for tracking of movement for new sockets
+		if(usersOnline == 0)
 		{
-			console.log(imgs[x]);
-			io.emit('loadImages', imgs[x]);
+			//DEBUG
+			console.log("Setting up initial puzzle pieces caching on server");
+
+			puzzlePiece.src = imgs[x];
+			puzzlePiece.id = x;
+			puzzlePiece.top = 0;
+			puzzlePiece.left = 0;
+
+			puzzlePieces.push(puzzlePiece);
 		}
 
-		io.emit('ImagesAreLoaded', imgs[x]);
+		//Broadcast initial image load for client
+		io.emit('loadImages', JSON.stringify(puzzlePieces[x]));
+	}
 
-	// 	isLoaded = 1;
-	// }
+	io.emit('ImagesAreLoaded', '');
 
+	// ON MOVE IMAGE BROADCAST
 	socket.on('moveImage', function(msg) {
-		io.emit('moveImage', msg)
-		console.log(msg);
+		
+		piece = JSON.parse(msg);
+		console.log('Updating piece coordinates on server: ');
+		console.log(piece.id+":");
+		console.log("	Top: " + piece.top +"");
+		console.log("	Left: " + piece.left +"");
+
+		puzzlePieces[piece.id].top = piece.top;
+		puzzlePieces[piece.id].left = piece.left;
+
+		io.emit('moveImage', msg);
 	});
+
+	socket.on('disconnect', function() {
+		usersOnline -= 1;
+	});
+
+	usersOnline += 1;
+
+	console.log("New user connected");
+	console.log("Users connected: " + usersOnline);
+
 });
 
 // GET IMAGE NAMES
-http.listen(3000, function()
+http.listen(process.env.PORT || 3000, function()
 {
 	console.log('Working...');
 
